afe7fd596f163966ea4b2a37481f26411e3658f8
danielus <danielandresus@gmail.com>
2024-08-18 11:05:05 -0400

    pawn valid start one or two forward rows player independent

--- chessboard.js
+++ chessboard.js
@@ -87,14 +87,74 @@ function changePlayer() {
 
 let dragId;
 let draggedElement;
+let dragLetter;
+let dragRow;
+let dragPlusOneRow;
+let dragPlusTwoRow;
 let movements = [];
 
 function dragStart(e) {
   dragId = e.target.parentNode.getAttribute("square-id");
   console.log("drag id: ", dragId);
+  dragLetter = dragId[0];
+  dragRow = dragId[1];
+  console.log("drag letter: ", dragId[0]);
+  console.log("drag row: ", dragId[1]);
+  // console.log("typeof drag id: ", typeof dragId);
   draggedElement = e.target;
   console.log("draggedElement", e.target);
   console.log("draggedElement id: ", draggedElement.getAttribute("id"));
+  console.log(dragId[0] + (+dragId[1] + 1));
+  console.log(playerTurn);
+
+  playerTurn === "white"
+    ? (dragPlusOneRow = dragId[0] + (+dragId[1] + 1))
+    : (dragPlusOneRow = dragId[0] + (+dragId[1] - 1));
+
+  playerTurn === "white"
+    ? (dragPlusTwoRow = dragId[0] + (+dragId[1] + 2))
+    : (dragPlusTwoRow = dragId[0] + (+dragId[1] - 2));
+
+  // startPlusOneRow = dragId[0] + (+dragId[1] + 1);
+  // startPlusTwoRows = dragId[0] + (+dragId[1] + 2);
+  console.log(dragPlusOneRow);
+  // console.log(typeof startPlusOneRow);
+  const forwardOne = document.querySelector(
+    `div[square-id = "${dragPlusOneRow}"]`
+  );
+  if (!forwardOne.firstChild) {
+    movements.push(dragPlusOneRow);
+  }
+  const forwardTwo = document.querySelector(
+    `div[square-id = "${dragPlusTwoRow}"]`
+  );
+  if (!forwardTwo.firstChild) {
+    movements.push(dragPlusTwoRow);
+  }
+  // const forwardOne = document.querySelector('div[square-id="h3"]'); // ok
+  // const forwardOne = document.getElementById("h3");
+  // const forwardOne = document.querySelector("h3");
+  // const forwardOne = document.querySelector(dragId[0] + (+dragId[1] + 1));
+  console.log(movements);
+
+  // const forwardOne = `${file}${rank + 1}`;
+  // const forwardTwo = `${file}${rank + 2}`;
+  // const captureLeft = `${String.fromCharCode(file.charCodeAt(0) - 1)}${
+  //   rank + 1
+  // }`;
+  // const captureRight = `${String.fromCharCode(file.charCodeAt(0) + 1)}${
+  //   rank + 1
+  // }`;
+
+  // // Regular move: one square forward
+  // if (isSquareEmpty(forwardOne, board)) {
+  //     moves.push(forwardOne);
+
+  //     // Double move: two squares forward (only from the initial position)
+  //     if (rank === 2 && isSquareEmpty(forwardTwo, board)) {
+  //         moves.push(forwardTwo);
+  //     }
+  //   }
 }
 
 function dragOver(e) {
@@ -141,6 +201,7 @@ function dragDrop(e) {
     // console.log("Valid move and take");
     // e.target.remove();
     // e.target.parentNode.append(draggedElement);
+    movements = [];
     changePlayer();
     return;
   }
@@ -154,9 +215,11 @@ function dragDrop(e) {
 
   if (e.target.childElementCount === 0) {
     e.target.append(draggedElement);
+    movements = [];
     changePlayer();
     return;
   }
+
   //   e.target.parentNode.append(draggedElement);
   //   e.target.remove();
   //   e.target.append(draggedElement);
