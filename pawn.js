83ae5a0e8f55e61f1df5f3d2a25537baaffff204
danielus <danielandresus@gmail.com>
2024-08-30 20:24:49 -0400

    pawn enpass refactored pend twosquares refactor

--- pawn.js
+++ pawn.js
@@ -6,9 +6,9 @@ function validPawnMoves() {
 
   // [column, row]
   const offsets = [
-    [0, 1], // up 1 row
-    [1, 1], // diagonal 1 right column & up 1 row obs.: white is in bottom, king is on the right of queen
-    [-1, 1], // diagonal 1 left column & up 1 row
+    [0, 1], //  1 row forward
+    [1, 1], // diagonal right column & 1 row forward. obs.: white is in bottom, king is on the right of queen
+    [-1, 1], // diagonal left column & 1 row forward
   ];
 
   const calculateMoves = ([colOffset, rowOffset]) => {
@@ -16,20 +16,15 @@ function validPawnMoves() {
     let currentRow = parseInt(dragRow);
     let isPreviousSquareEmpty = false;
 
-    currentRow += rowOffset * opponentPiece;
     currentCol += colOffset * opponentPiece;
+    currentRow += rowOffset * opponentPiece;
 
     let offsetId = String.fromCharCode(currentCol) + currentRow;
     console.log(offsetId);
 
     if (coords.includes(offsetId)) {
       let squareOffset = getDivOffset(offsetId);
-      // console.log(draggedDiv);
-      // let isOneSquareEmpty = false; // maybe not necessary put false just let ...
-      // let squareId = String.fromCharCode(currentCol) + currentRow;
-      // let square = document.querySelector(`div[square-id = "${squareId}"]`);
-      // console.log(square); // <div class="square light" square-id="h3">
-      // console.log(squareId);
+
       let isSquareEmpty = isEmpty(squareOffset);
       console.log(`Is `, offsetId, ` empty? `, isSquareEmpty); // undefined -> false
 
@@ -59,65 +54,18 @@ function validPawnMoves() {
         console.log(moves);
       }
 
-      // En passant right
-      // col < "h".charCodeAt(0)
-      if (
-        String.fromCharCode(currentCol) <= "h" &&
-        colOffset * opponentPiece === 1
-      ) {
-        // if (currentCol <= "h" && colOffset * opponentPiece === 1) {
-        // if (colOffset <= "h" && colOffset * opponentPiece === 1) {
-        // if (colOffset < "h" && colOffset === 1) {
-        const squareRightId =
-          String.fromCharCode(dragCol.charCodeAt(0) + 1) + dragRow;
-        const squareRight = document.querySelector(
-          `div[square-id = "${squareRightId}"]`
-        );
-        const enpassRight =
-          squareRight.firstChild?.classList.contains("enpass"); // If is true then it could only be opposite pawn
+      // En passant
+      if (isSquareEmpty && colOffset != 0) {
+        let colOffsetId = String.fromCharCode(currentCol) + dragRow;
 
-        // Diagonal right forward empty but with pawn besides right with enpass class
-        if (isSquareEmpty && enpassRight) {
-          moves.push(offsetId);
-        }
+        let colOffset = getDivOffset(colOffsetId);
+
+        let colOffsetEnpass =
+          colOffset.firstChild?.classList.contains("enpass");
+        console.log(`Is rightLeft Enpass? `, colOffsetEnpass);
+        colOffsetEnpass && moves.push(offsetId); // undefined -> false
       }
 
-      // En passant left
-      // col < "h".charCodeAt(0)
-      console.log(`current col: `, currentCol);
-      console.log(`row offset: `, rowOffset);
-      console.log(`row offset ok: `, rowOffset * opponentPiece);
-      console.log(`col offset: `, colOffset);
-      console.log(`col offset: ok `, colOffset * opponentPiece);
-      console.log(
-        `current col > 'a'.charCodeAt(0) (97): `,
-        currentCol > "a".charCodeAt(0)
-      );
-      console.log(`col offset === -1: `, colOffset === -1);
-      console.log(
-        `col rowOffset * opponentPiece === -1: `,
-        rowOffset * opponentPiece === -1
-      );
-      if (
-        String.fromCharCode(currentCol) >= "a" &&
-        colOffset * opponentPiece === -1
-      ) {
-        // if (currentCol >= "a".charCodeAt(0) && colOffset * opponentPiece === -1) {
-        const squareLeftId =
-          String.fromCharCode(dragCol.charCodeAt(0) - 1) + dragRow;
-        console.log(squareLeftId);
-        const squareLeft = document.querySelector(
-          `div[square-id = "${squareLeftId}"]`
-        );
-        console.log(squareLeft);
-        const enpassLeft = squareLeft.firstChild?.classList.contains("enpass"); // If is true then it could only be opposite pawn
-        console.log(enpassLeft);
-        // Diagonal left forward empty but with pawn besides left with enpass class
-        // enpassLeft && moves.push(squareLeftId)
-        if (isSquareEmpty && enpassLeft) {
-          moves.push(offsetId);
-        }
-      }
       currentRow += rowOffset * opponentPiece;
       console.log(currentRow);
       offsetId = String.fromCharCode(currentCol) + currentRow;
@@ -127,10 +75,7 @@ function validPawnMoves() {
       // squareOffset = document.querySelector(`div[square-id = "${offsetId}"]`);
       console.log(squareOffset); // <div class="square light" square-id="h3">
       console.log(offsetId);
-      // offsetId = String.fromCharCode(currentCol) + currentRow;
-      // squareOffset = document.querySelector(`div[square-id = "${offsetId}"]`);
-      // console.log(squareOffset); // <div class="square light" square-id="h3">
-      // console.log(offsetId);
+
       isSquareEmpty = isEmpty(squareOffset);
       console.log(`Is `, offsetId, ` empty? `, isSquareEmpty); // true
 
